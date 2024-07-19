9bc81de71ee46ff8dd6bf77de55252fa8d2e895b
Matthias Beyer <mail@beyermatthias.de>
2024-07-19 18:46:21 +0200

    Fix: Add missing clap attributes
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/cli.rs
+++ src/cli.rs
@@ -3,8 +3,15 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 pub struct Cli {
+    #[clap(short, long)]
     pub verbose: bool,
+
+    #[clap(short, long)]
     pub debug: bool,
+
+    #[clap(short, long)]
     pub trace: bool,
+
+    #[clap(short, long)]
     pub config: Utf8PathBuf,
 }
